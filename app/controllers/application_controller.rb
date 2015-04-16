f43236ed91a6b43737cfd811ac8161b5a0ac7427
Bob Bonifield <bobbonifield@gmail.com>
2015-04-16 13:59:54 -0600

    Customizing application controller
    - only listen to JSON requests
    - global error handling for strong parameter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
+  include ActionController::ImplicitRender
+
+  before_filter :check_format
+
+  def check_format
+    render :nothing => true, :status => 406 unless params[:format] == 'json' || request.headers["Accept"] =~ /json/
+  end
+
+  rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
+    error = "Missing required paramater: #{parameter_missing_exception.param}"
+    response = { error: error }
+    render json: response, status: :unprocessable_entity
+  end
 end
