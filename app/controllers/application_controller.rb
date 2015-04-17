2cc1abc84b4fa1c8c1e35ed8b900d291a27ca3f4
Bob Bonifield <bobbonifield@gmail.com>
2015-04-17 09:11:03 -0600

    Style and comment update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,12 @@ class ApplicationController < ActionController::API
 
   before_filter :check_format
 
+  # force JSON
   def check_format
-    render :nothing => true, :status => 406 unless params[:format] == 'json' || request.headers["Accept"] =~ /json/
+    render nothing: true, status: 406 unless params[:format] == 'json' || request.headers["Accept"] =~ /json/
   end
 
+  # custom error message from strong paramaters
   rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
     error = "Missing required paramater: #{parameter_missing_exception.param}"
     response = { error: error }
