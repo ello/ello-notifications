a39eb7afb5bb64f607edaaf2bcbf62866869a35a
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-07-08 16:54:15 -0600

    Add marketing/build version to push subscriptions.

--- app/interactors/create_device_subscription.rb
+++ app/interactors/create_device_subscription.rb
@@ -20,7 +20,9 @@ class CreateDeviceSubscription
     result = APNS::CreateSubscription.call({
       platform_device_identifier: request.platform_device_identifier,
       bundle_identifier: request.bundle_identifier,
-      logged_in_user_id: request.logged_in_user_id
+      logged_in_user_id: request.logged_in_user_id,
+      marketing_version: request.marketing_version,
+      build_version: request.build_version
     })
 
     context.fail!(message: result.message, failure_reason: result.failure_reason) if result.failure?
