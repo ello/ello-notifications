2c34bfa264b5b2d677f3c4e66f9b9d0e4937bd55
Sean Dougherty <seancdougherty@gmail.com>
2016-08-25 17:48:56 -0600

    Removes unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  scope '/apns' do
-    resources :apns_subscriptions,
-      path: 'subscriptions',
-      param: :platform_device_identifier,
-      defaults: { format: :json },
-      only: [:create, :destroy]
-  end
-
   scope '/device_subscriptions' do
     post 'create' => 'device_subscriptions#create', as: :create_device_subscription
     post 'delete' => 'device_subscriptions#destroy', as: :delete_device_subscription
