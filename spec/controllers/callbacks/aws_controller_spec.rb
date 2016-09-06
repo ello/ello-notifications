3691111afc3e5c29d6a9e2d60aa35fd2d1993789
Sean Dougherty <seancdougherty@gmail.com>
2016-09-06 13:18:43 -0600

    Fixes a bunch of hound warnings

--- spec/controllers/callbacks/aws_controller_spec.rb
+++ spec/controllers/callbacks/aws_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe Callbacks::AwsController, type: :request do
+  Basic = ActionController::HttpAuthentication::Basic
 
   before do
     allow_any_instance_of(Aws::SNS::Client).to receive(:confirm_subscription)
@@ -11,7 +12,7 @@ describe Callbacks::AwsController, type: :request do
 
   describe 'POST #push_failed' do
 
-    context 'when verifying subscription' do
+    context "when verifying subscription" do
 
       before do
         allow_any_instance_of(Aws::SNS::Client).to receive(:confirm_subscription).and_return(confirmation_response)
@@ -19,87 +20,100 @@ describe Callbacks::AwsController, type: :request do
 
       let(:body) {{ Token: '123', TopicArn: '456'}}
       let(:headers) do
-        {'x-amz-sns-message-type' => 'SubscriptionConfirmation',
+        {
+          'x-amz-sns-message-type' => 'SubscriptionConfirmation',
           'Content-Type' => 'application/json',
-          'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials('admin','admin')
+          'HTTP_AUTHORIZATION' => Basic.encode_credentials('admin','admin')
         }
       end
 
-      context 'when the verification is successful' do
-        let(:confirmation_response) { instance_double("Seahorse::Client::Response", successful?: true) }
+      context "when the verification is successful" do
+        let(:confirmation_response) {
+          instance_double("Seahorse::Client::Response", successful?: true)
+        }
 
-        it 'returns a 200' do
+        it "returns a 200" do
           post '/callbacks/aws/push_failed', body.to_json, headers
           expect(response.status).to be(200)
         end
       end
 
-      context 'when the verification is unsuccessful' do
-        let(:confirmation_response) { instance_double("Seahorse::Client::Response", successful?: false) }
+      context "when the verification is unsuccessful" do
+        let(:confirmation_response) {
+          instance_double("Seahorse::Client::Response", successful?: false)
+        }
 
-        it 'returns a 406' do
+        it "returns a 406" do
           post '/callbacks/aws/push_failed', body.to_json, headers
           expect(response.status).to be(406)
         end
       end
     end
 
-    context 'with a validated message' do
+    context "with a validated message" do
 
       before do
         allow_any_instance_of(Aws::SNS::MessageVerifier).to receive(:authentic?).and_return(true)
       end
 
-      context 'message is a failed push notification' do
-        let!(:device_subscription) { create(:device_subscription, :gcm, platform_device_identifier: '123') }
+      context "message is a failed push notification" do
+        let!(:subscription) {
+          create(:device_subscription, :gcm, platform_device_identifier: '123')
+        }
 
         let(:headers) do
-          {'x-amz-sns-message-type' => 'Notification',
-           'Content-Type' => 'application/json',
-           'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials('admin','admin')
+          {
+            'x-amz-sns-message-type' => 'Notification',
+            'Content-Type' => 'application/json',
+            'HTTP_AUTHORIZATION' => Basic.encode_credentials('admin','admin')
           }
         end
 
         let(:body) {{ delivery: { token: '123' }, status: 'FAILURE'}}
 
-        it 'deletes the device subscription' do
-          expect(DeviceSubscription.find(device_subscription.id)).to be_truthy
+        it "deletes the device subscription" do
+          expect(DeviceSubscription.find(subscription.id)).to be_truthy
           post '/callbacks/aws/push_failed', body.to_json, headers
           expect(response.status).to be(200)
-          expect(DeviceSubscription.find_by(id: device_subscription.id)).to be_nil
+          expect(DeviceSubscription.find_by(id: subscription.id)).to be_nil
         end
       end
 
-      context 'message is NOT a failed push notification' do
+      context "message is NOT a failed push notification" do
 
-        let!(:device_subscription) { create(:device_subscription, :gcm, platform_device_identifier: '123') }
+        let!(:subscription) {
+          create(:device_subscription, :gcm, platform_device_identifier: '123')
+        }
 
         let(:headers) do
           {
            'Content-Type' => 'application/json',
-           'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials('admin','admin')
+           'HTTP_AUTHORIZATION' => Basic.encode_credentials('admin','admin')
           }
         end
 
         let(:body) {{ deliver: { token: '123' }, status: 'NOT FAILURE'}}
 
-        it 'does not delete a device subscription' do
-          expect(DeviceSubscription.find(device_subscription.id)).to be_truthy
+        it "does not delete a device subscription" do
+          expect(DeviceSubscription.find(subscription.id)).to be_truthy
           post '/callbacks/aws/push_failed', body.to_json, headers
           expect(response.status).to be(200)
-          expect(DeviceSubscription.find(device_subscription.id)).to be_truthy
+          expect(DeviceSubscription.find(subscription.id)).to be_truthy
         end
       end
     end
 
-    context 'with an unvalidated message' do
+    context "with an unvalidated message" do
 
-      let!(:device_subscription) { create(:device_subscription, :gcm, platform_device_identifier: '123') }
+      let!(:subscription) {
+        create(:device_subscription, :gcm, platform_device_identifier: '123')
+      }
 
       let(:headers) do
-        {'x-amz-sns-message-type' => 'Notification',
+        {
+          'x-amz-sns-message-type' => 'Notification',
           'Content-Type' => 'application/json',
-          'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials('admin','admin')
+          'HTTP_AUTHORIZATION' => Basic.encode_credentials('admin','admin')
         }
       end
 
@@ -107,11 +121,11 @@ describe Callbacks::AwsController, type: :request do
         allow_any_instance_of(Aws::SNS::MessageVerifier).to receive(:authentic?).and_return(false)
       end
 
-      it 'does not delete a device subscription' do
-        expect(DeviceSubscription.find(device_subscription.id)).to be_truthy
+      it "does not delete a device subscription" do
+        expect(DeviceSubscription.find(subscription.id)).to be_truthy
         post '/callbacks/aws/push_failed', body.to_json, headers
         expect(response.status).to be(406)
-        expect(DeviceSubscription.find(device_subscription.id)).to be_truthy
+        expect(DeviceSubscription.find(subscription.id)).to be_truthy
       end
     end
   end
