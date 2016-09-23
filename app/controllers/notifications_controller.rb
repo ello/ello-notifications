09a1fdfc9549e785fa1c29e1467e4f09dfd84862
Jay Zeschin <jay.zeschin@modeset.com>
2016-09-23 15:30:53 -0600

    Give Honeybadger the protobuf request as context

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,9 +1,8 @@
 class NotificationsController < ApplicationController
   def create
     protobuf_request = ElloProtobufs::NotificationService::CreateNotificationRequest.decode_from(request.body)
-    result = CreateNotification.call({
-      request: protobuf_request
-    })
+    Honeybadger.context(protobuf_request: protobuf_request)
+    result = CreateNotification.call(request: protobuf_request)
 
     resp = service_response(result)
     render_protobuf_response(resp)
